bdad95544847741340d9bc988e84e960c60a5583
Matias Guerra <mat.guerraj@duocuc.cl>
2026-06-21 14:01:31 -0400

    Align soporte model with ticket diagram

--- src/main/java/microservice/soporte/repository/ReporteInventarioRepository.java
+++
@@ -1,9 +0,0 @@
-package microservice.soporte.repository;
-
-import microservice.soporte.model.ReporteInventario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReporteInventarioRepository extends JpaRepository<ReporteInventario, Long> {
-}
