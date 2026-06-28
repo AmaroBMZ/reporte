f7f1669d76ddceaa88e646b4858febad8f417648
Matias Guerra <mat.guerraj@duocuc.cl>
2026-06-28 15:01:22 -0400

    mejora pruebas de cobertura

--- src/main/java/microservice/reporte/ReporteApplication.java
+++ src/main/java/microservice/reporte/ReporteApplication.java
@@ -1,8 +1,8 @@
 package microservice.reporte;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
